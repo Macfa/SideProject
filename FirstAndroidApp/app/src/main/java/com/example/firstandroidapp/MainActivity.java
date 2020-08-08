b901a2e3d33b46ad13df31a98b80f872603c3144
ahsol <ahsol@ahsolui-MacBookPro.local>
2020-08-08 14:45:12 +0900

    Grid Successfull

--- FirstAndroidApp/app/src/main/java/com/example/firstandroidapp/MainActivity.java
+++ FirstAndroidApp/app/src/main/java/com/example/firstandroidapp/MainActivity.java
@@ -13,27 +13,39 @@ import static android.view.Gravity.CENTER;
 
 public class MainActivity extends AppCompatActivity {
 
+    private GridLayout Main_GL_Items;
+    private TextView Header_TV_;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GridLayout MainMenuList = new GridLayout(this);
-        MainMenuList.setOrientation(GridLayout.HORIZONTAL);
-        MainMenuList.setRowCount(2);
-        MainMenuList.setColumnCount(2);
+        Main_GL_Items = findViewById(R.id.Main_GL_Items);
 
+        //GridLayout MainMenuList = new GridLayout(this);
+        Main_GL_Items.setOrientation(GridLayout.HORIZONTAL);
+        Main_GL_Items.setRowCount(1);
+        Main_GL_Items.setColumnCount(1);
+
+        /*
         GridLayout.LayoutParams MainMenuListParams = new GridLayout.LayoutParams();
         MainMenuListParams.setGravity(CENTER);
-        MainMenuListParams.height = GridLayout.LayoutParams.WRAP_CONTENT;
+        MainMenuListParams.height = GridLayout.LayoutParams.MATCH_PARENT;
         MainMenuListParams.width = GridLayout.LayoutParams.MATCH_PARENT;
 
+    */
+
+
         TextView txt = new TextView(this);
+        txt.setHeight(500);
+        txt.setWidth(500);
         txt.setText("asdasdasd");
-        MainMenuList.addView(txt, MainMenuListParams);
 
 
-        MainMenuList.setLayoutParams(MainMenuListParams);
+
+        //MainMenuList.setLayoutParams(MainMenuListParams);
+        Main_GL_Items.addView(txt);
 
     }
 }
